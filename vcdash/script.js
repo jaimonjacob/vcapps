94f680799301afda6173253e8dd99f0d5c939bc4
jaimonjacob <jaimon.jacob2009@gmail.com>
2020-05-11 14:48:23 +0000

    adf

--- vcdash/script.js
+++ vcdash/script.js
@@ -17,7 +17,8 @@ lrs.queryStatements({
     verb: new TinCan.Verb({
       id: 'http://adlnet.gov/expapi/verbs/completed',
     }),
-    since: '2020-05-10T08:34:16Z',
+    since: '2020-05-11T14:47:20+0000',
+    //2020-05-10T08:34:16Z
   },
   callback: function (err, sr) {
     if (err !== null) {
