7baf815d2f452d2f01c59fe21721ff4d0eca8792
jaimonjacob <jaimon.jacob2009@gmail.com>
2020-05-11 06:07:37 +0000

    green

--- vcdash/script.js
+++ vcdash/script.js
@@ -1,29 +1,30 @@
+var tincan = new  TinCan({
+		recordStores: [{
+		endpoint: 'https://watershedlrs.com/api/organizations/10981/lrs/',
+		username: 'bde27ce0f7b36e',
+		password: '2bb3f290d84811',
+		allowFail:false
+		}]
+	});
+  var MultiArray = []
+ 	tincan.getStatements({
 
-var xhr = new XMLHttpRequest();
-xhr.addEventListener('load', function () {
-  if (this.status == 200) {
-    var response = JSON.parse(this.responseText);
-    var statements = response.statements;
-    console.log(statements)
-    var statementsData = [];
-    statements.forEach(function (statement) {
-           
-      if (statement.verb.id !== "http://adlnet.gov/expapi/verbs/voided") {
-        var valueOne = statement.actor.name;
-        delete valueOne.mbox;
-        delete valueOne.objectType;
+	  'params' : {
+		'verb' : {
+			'id' : 'http://adlnet.gov/expapi/verbs/completed'
+		 }
+	  }, // <-- don't forget this comma, seriously, it will drive you nuts.
+	  'callback': function (err, result) {
+		      
+      //console.log(result); 
+      var statements = result.statements
+      //console.log(statements)   
+      var statementsData = [];
+      statements.forEach(function (statement) {          
+         statementsData.push([statement.actor.name, statement.result.score.raw]);           
+      });
 
-        var valueThree = statement.verb.display["en-US"];
-        var valuTwo = statement.result.score.scaled;
-        statementsData.push([valueOne, valuTwo, valueThree]);
-      
-      }
-    });
-
-    console.log(statementsData)
-
-
-    var sum = {}, summativeData;
+      var sum = {}, summativeData;
 
     for (var i = 0, c; c = statementsData[i]; ++i) {
       if (undefined === sum[c[0]]) {
@@ -35,7 +36,7 @@ xhr.addEventListener('load', function () {
     }
     summativeData = Object.keys(sum).map(function (val) { return sum[val] });
 
-    console.log(summativeData);
+    //console.log(summativeData);
 
     var xValues = summativeData.map(function(x){
     return x[0];
@@ -44,29 +45,10 @@ xhr.addEventListener('load', function () {
     var yValues = summativeData.map(function(x){
       return x[1];
     })
-    console.log(xValues);
-    console.log(yValues);
-   //Chart Code Here
-/* 
-   var data = [
-    {
-      x: summativeData.map(function(x){
-        return x[1]
-        }),
+    //console.log(xValues);
+    //console.log(yValues);
 
-      y: summativeData.map(function(x){
-        return x[0]
-        }),
-      type: 'bar',
-      orientation: 'h'
-    }
-  ];
-  
-  Plotly.newPlot('myDiv', data);
-
-     */
-
-  var myChart = document.getElementById("myChart").getContext("2d");
+    var myChart = document.getElementById("myChart").getContext("2d");
   var barChart = new Chart(myChart, {
     type: "bar",
     data: {
@@ -74,7 +56,7 @@ xhr.addEventListener('load', function () {
       datasets: [{
         label: "score",
         data: yValues,
-        backgroundColor: "#0091DA",
+        backgroundColor: "#483698",
         borderWidth: 1,
         borderColor: "#00338D",
         hoverBorderWidth: 2,
@@ -84,11 +66,16 @@ xhr.addEventListener('load', function () {
     options: {
       title:{
         display: true,
-        text: "The score dashboard",
+        text: "score dashboard", 
+        fontColor:  '#E5E2E8',      
         fontSize: 25
       },
       legend: {
-        position: "right"
+        position: "right",
+        labels: {
+
+                fontColor: 'white'
+            }
       },
       layout: {
         padding: 20
@@ -99,20 +86,29 @@ xhr.addEventListener('load', function () {
       scales: {
         yAxes: [{
             ticks: {
-                suggestedMin: 10,
-                suggestedMax: 50
+                suggestedMin: 5,
+                suggestedMax: 50,
+                fontColor: '#E5E2E8'
+            }
+        }],
+
+    xAxes: [{
+            ticks: {
+                fontColor: '#E5E2E8',
             }
         }]
+
+
+
     }
     }
 
 
   })
 
-   //Chart code above 
-  }
-});
-xhr.open('GET', 'https://watershedlrs.com/api/organizations/10981/lrs/statements');
-xhr.setRequestHeader("x-experience-api-version", "1.0.3");
-xhr.setRequestHeader("authorization", "Basic YmRlMjdjZTBmN2IzNmU6MmJiM2YyOTBkODQ4MTE=");
-xhr.send(); 
+    
+    
+    
+    }
+	})
+
